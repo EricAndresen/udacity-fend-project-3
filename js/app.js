abfe993cbe06b43dc09714da29496db5eb414e99
Eric-Andresen <erandre7@gmail.com>
2018-04-02 11:03:19 -0700

    Add Player Class

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    consol.log("enemy moved");
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,12 +26,28 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function() {
+    this.sprite = "images/char-boy.png";
+}
+
+Player.prototype.update = function(dt){
+    console.log("player moved");
+    // will update this.x and this.y
+}
+
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+let enemyOne = new Enemy();
+let player = new Player();
 
+let allEnemies = []
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
